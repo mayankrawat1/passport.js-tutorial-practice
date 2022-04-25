2a9e581f5909a146c3740b848b85263a90a95879
mayank <mayank.rawat1@tothenew.com>
2022-04-25 11:46:39 +0530

    done frontend part

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const path = require("path");
+const authRoute = require("./routes/authRoute");
 const app = express();
 const port = 3000 || process.env.PORT;
 
+//views path
+const viewPath = path.join(__dirname, "./views");
+
+//set template engine
+app.set("views", viewPath);
+app.set("view engine", "ejs");
+
+//middleware or routes handle
+app.use("/auth", authRoute);
+
 app.get("/", (req, res) => {
-  res.send("Home page");
+  res.render("home");
 });
 
 app.listen(port, (req, res) => {
